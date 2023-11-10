cb4f96269b49e753408d48c81f27a21efbfa7c26
JoaoVNat <joaovictor567bmsk8@hotmail.com>
2023-11-10 15:24:09 -0300

    first commit

---
+++ AppPessoa.java
@@ -0,0 +1,28 @@
+package J.Martins;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class AppPessoa {
+    public static void main(String[] args) {
+        List<PessoaGrupo> listPessoas = new PessoaGrupo().listaPessoaGP();
+        System.out.println("Grupo Masculino; ");
+        List<PessoaGrupo> listMain = listPessoas.stream()
+                .filter(pessoaGrupo -> pessoaGrupo.getGenero().equals("Masculino"))
+                .collect(Collectors.toList());
+
+        listMain.forEach(System.out::println);
+
+        System.out.println("*************");
+        System.out.println("*************");
+        System.out.println("*************");
+
+        System.out.println("Grupo Feminino: ");
+        listPessoas.stream()
+                .filter(pessoaGrupo -> pessoaGrupo.getGenero().equals("Feminino"))
+                .collect(Collectors.toList())
+                .forEach(System.out::println);
+
+
+    }
+}
